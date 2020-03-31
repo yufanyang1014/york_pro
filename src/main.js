ab369547f31c577d90e9905011f34744ab7a43db
yangyufan <yangyufan@ibuycionhd.com>
2020-03-31 14:26:24 +0800

    feat: serviceWorker

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
+import './registerServiceWork';
 
 Vue.config.productionTip = false;
 
